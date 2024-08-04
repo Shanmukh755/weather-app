1090a173854e8011c5ebdbe3a7785fb7284b6c16
shanmukh755 <thalabathulasurishanmukha@gmail.com>
2024-08-05 04:08:32 +0530

    error handling

--- controller/CityController.js
+++ controller/CityController.js
@@ -1,7 +1,7 @@
 const CityModel = require('../model/City')
 const https = require('https')
 
-
+//main.temp main.humidity name weather[0].description wind.speed 
 const getCityWeather = async (req, res)=>{
     try{
         const {city} = req.body
@@ -10,13 +10,27 @@ const getCityWeather = async (req, res)=>{
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
         https.get(url, (response)=>{
             response.on('data', (data)=>{
-                const weatherData = JSON.parse(data)
-                console.log(weatherData)
-                res.status(201).json(weatherData)
-            })
+                try{
+                    const weatherData = JSON.parse(data)
+                    console.log(weatherData)
+                    res.status(201).json({
+                        temperature: weatherData.main.temp,
+                        humidity: weatherData.main.humidity,
+                        city: weatherData.name,
+                        description: weatherData.weather[0].description,
+                        windSpeed: weatherData.wind.speed,
+                        status: true
+                    })
+                } catch (err) {
+                    res.status(500).json({message: 'Failed to parse weather data', status: false})
+                }
+            }).on('error', (e) => {
+                res.status(500).json({ message: 'Error fetching weather data' , status: false});
+            });
+    
         })
     } catch (err) {
-        res.send(401).json({message: '401 NOT FOUND',})
+        res.status(401).json({message: '401 NOT FOUND', status: false})
     }
 }
 
